4713d19d9ce774ce5aefe4d5b18e883e88dff7ef
David zhang <d.ydzdavid@yahoo.com>
2019-03-04 14:32:22 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,12 +1,9 @@
 class Dog
-  def initialize (name, breed)
-    @name = name 
-    @breed = breed
-  end
-  
   def name
     @name = name
   end
+  
+  def name=(name)
   
   def breed 
     @breed = breed
